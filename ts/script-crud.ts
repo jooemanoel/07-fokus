6969163e1bb6f787ec24f2c9d9885e3af3ed0335
F4844758 Joao Emanoel Rodrigues Mota <joao_mota.jm@bb.com.br>
2024-08-09 14:21:51 -0300

    Final

--- ts/script-crud.ts
+++ ts/script-crud.ts
@@ -5,7 +5,8 @@ interface Tarefa {
 
 interface EstadoAplicacao {
     tarefas: Tarefa[]
-    tarefaSelecionada: Tarefa | null
+    tarefaSelecionada: Tarefa | null,
+    editando: boolean
 }
 
 let estadoInicial: EstadoAplicacao = {
@@ -23,7 +24,8 @@ let estadoInicial: EstadoAplicacao = {
             concluida: false
         }
     ],
-    tarefaSelecionada: null
+    tarefaSelecionada: null,
+    editando: false
 }
 
 const selecionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao => {
@@ -41,6 +43,32 @@ const adicionarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicac
     }
 }
 
+// Deleta uma tarefa. Retorna um novo estado.
+const deletar = (estado: EstadoAplicacao): EstadoAplicacao => {
+    if (estado.tarefaSelecionada) {
+        const tarefas = estado.tarefas.filter(t => t != estado.tarefaSelecionada);
+        return { ...estado, tarefas, tarefaSelecionada: null, editando: false };
+    } else {
+        return estado;
+    }
+}
+
+// Deleta todas as tarefas. Retorna um novo estado.
+const deletarTodas = (estado: EstadoAplicacao): EstadoAplicacao => {
+    return { ...estado, tarefas: [], tarefaSelecionada: null, editando: false };
+}
+
+// Deleta todas as tarefas concluídas. Retorna um novo estado.
+const deletarTodasConcluidas = (estado: EstadoAplicacao): EstadoAplicacao => {
+    const tarefas = estado.tarefas.filter(t => !t.concluida);
+    return { ...estado, tarefas, tarefaSelecionada: null, editando: false };
+}
+
+// Modifica o estado para entrar no modo de edição. Retorna um novo estado.
+const editarTarefa = (estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao => {
+    return { ...estado, editando: !estado.editando, tarefaSelecionada: tarefa };
+}
+
 const atualizarUI = () => {
     const taskIconSvg = `
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -55,6 +83,22 @@ const atualizarUI = () => {
     const formAdicionarTarefa = document.querySelector<HTMLFormElement>('.app__form-add-task')
     const btnAdicionarTarefa = document.querySelector<HTMLButtonElement>('.app__button--add-task')
     const textarea = document.querySelector<HTMLTextAreaElement>('.app__form-textarea')
+    const labelTarefaAtiva = document.querySelector<HTMLParagraphElement>('.app__section-active-task-description')
+    const btnCancelar: HTMLButtonElement = document.querySelector('.app__form-footer__button--cancel') as HTMLButtonElement
+    const btnDeletar: HTMLButtonElement = document.querySelector('.app__form-footer__button--delete') as HTMLButtonElement
+
+    const btnDeletarConcluidas: HTMLButtonElement = document.querySelector('#btn-remover-concluidas') as HTMLButtonElement
+    const btnDeletarTodas: HTMLButtonElement = document.querySelector('#btn-remover-todas') as HTMLButtonElement
+
+    labelTarefaAtiva!.textContent = estadoInicial.tarefaSelecionada ? estadoInicial.tarefaSelecionada.descricao : null
+
+    if (estadoInicial.editando && estadoInicial.tarefaSelecionada) {
+        formAdicionarTarefa!.classList.remove('hidden')
+        textarea!.value = estadoInicial.tarefaSelecionada.descricao
+    } else {
+        formAdicionarTarefa!.classList.add('hidden')
+        textarea!.value = ''
+    }
 
     if (!btnAdicionarTarefa) {
         throw Error("Caro colega, o elemento btnAdicionarTarefa não foi encontrado. Favor rever.")
@@ -72,6 +116,26 @@ const atualizarUI = () => {
             concluida: false
         })
         atualizarUI()
+    }
+
+    btnCancelar.onclick = () => {
+        formAdicionarTarefa!.classList.add('hidden');
+    }
+
+    btnDeletar.onclick = () => {
+        estadoInicial = deletar(estadoInicial);
+        formAdicionarTarefa!.classList.add('hidden');
+        atualizarUI();
+    }
+
+    btnDeletarConcluidas.onclick = () => {
+        estadoInicial = deletarTodasConcluidas(estadoInicial);
+        atualizarUI();
+    }
+
+    btnDeletarTodas.onclick = () => {
+        estadoInicial = deletarTodas(estadoInicial);
+        atualizarUI();
     }
 
     if (ulTarefas) {
@@ -102,6 +166,10 @@ const atualizarUI = () => {
             li.classList.add('app__section-task-list-item-complete')
         }
 
+        if (tarefa == estadoInicial.tarefaSelecionada) {
+            li.classList.add('app__section-task-list-item-active')
+        }
+
         li.appendChild(svgIcon)
         li.appendChild(paragraph)
         li.appendChild(button)
@@ -112,8 +180,22 @@ const atualizarUI = () => {
             atualizarUI()
         })
 
+        // Adicionar evento de clique para editar uma tarefa
+        editIcon.onclick = (evento) => {
+            evento.stopPropagation();
+            estadoInicial = editarTarefa(estadoInicial, tarefa);
+            atualizarUI();
+        }
+
         ulTarefas?.appendChild(li)
     })
 }
+
+document.addEventListener('TarefaFinalizada', () => {
+    if (estadoInicial.tarefaSelecionada) {
+        estadoInicial.tarefaSelecionada.concluida = true
+        atualizarUI()
+    }
+})
 
 atualizarUI()
